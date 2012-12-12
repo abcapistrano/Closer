b534531563dbafabd5e62c2a355ce9ec0d25830a
Earl <earltagra@Bolt.local>
2012-12-12 22:15:43 +0800

    Initial Commit

---
+++ Closer/DJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DJAppDelegate.h
+//  Closer
+//
+//  Created by Earl on 12/12/12.
+//  Copyright (c) 2012 Earl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DJAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Closer/DJAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DJAppDelegate.m
+//  Closer
+//
+//  Created by Earl on 12/12/12.
+//  Copyright (c) 2012 Earl. All rights reserved.
+//
+
+#import "DJAppDelegate.h"
+
+@implementation DJAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
